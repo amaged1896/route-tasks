95f44540670414bd3764e3475d64c63b0b1524fd
amaged1896 <ahmed.maged1896@gmail.com>
2022-09-30 22:12:29 +0200

    Javascript Assignment Num-3 "bookmarker"

---
+++ Front-end Assignmets/task-9/js/script.js
@@ -0,0 +1,44 @@
+var siteNameInput = document.getElementById("siteNameInput");
+var urlInput = document.getElementById("urlInput");
+var webSiteInfo = [];
+
+if (localStorage.getItem("website") != null) {
+  webSiteInfo = JSON.parse(localStorage.getItem("website"));
+  displayWebSites();
+}
+
+function addWebSite() {
+  var website = {
+    siteName: siteNameInput.value,
+    siteUrl: urlInput.value,
+  };
+  webSiteInfo.push(website);
+  localStorage.setItem("website", JSON.stringify(webSiteInfo));
+  displayWebSites();
+  clearInputs();
+}
+
+function displayWebSites() {
+  var sites = ``;
+  for (i = 0; i < webSiteInfo.length; i++) {
+    sites += `<tr>
+          <td>${webSiteInfo[i].siteName}</td>
+          <td>${webSiteInfo[i].siteUrl}</td>
+          
+          <td><button onclick="deleteSite(${i})" class="btn btn-sm btn-danger">Delete</button></td>
+          <td><a href="${webSiteInfo[i].siteUrl}" class="btn btn-sm btn-primary" target="_blank">visit</a></td>
+          </tr>`;
+  }
+  document.getElementById("tableBody").innerHTML = sites;
+}
+
+function clearInputs() {
+  siteNameInput.value = "";
+  urlInput.value = "";
+}
+
+function deleteSite(deletedIndex) {
+  webSiteInfo.splice(deletedIndex, 1);
+  localStorage.setItem("website", JSON.stringify(webSiteInfo));
+  displayWebSites();
+}
