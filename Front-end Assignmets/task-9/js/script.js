4ebac5fd5d527ca4e6f69ba6a96b98df7a64cf03
amaged1896 <ahmed.maged1896@gmail.com>
2022-10-09 21:10:40 +0200

    "bookmarker App add - display - delete - clear - validate - update "

--- Front-end Assignmets/task-9/js/script.js
+++ Front-end Assignmets/task-9/js/script.js
@@ -1,43 +1,130 @@
-var siteNameInput = document.getElementById("siteNameInput");
-var urlInput = document.getElementById("urlInput");
-var webSiteInfo = [];
+let websiteNameInput = document.getElementById("websiteNameInput");
+let websiteURL = document.getElementById("websiteURLInput");
+let webSiteInfo = [];
+let currentIndex = 0;
 
 if (localStorage.getItem("website") != null) {
   webSiteInfo = JSON.parse(localStorage.getItem("website"));
-  displayWebSites();
+  displayWebsite();
 }
 
-function addWebSite() {
-  var website = {
-    siteName: siteNameInput.value,
-    siteUrl: urlInput.value,
-  };
-  webSiteInfo.push(website);
-  localStorage.setItem("website", JSON.stringify(webSiteInfo));
-  displayWebSites();
-  clearInputs();
-}
+// Clear Inputs
 
-function displayWebSites() {
-  var sites = ``;
-  for (i = 0; i < webSiteInfo.length; i++) {
-    sites += `<tr>
-          <td>${webSiteInfo[i].siteName}</td>
-          <td>${webSiteInfo[i].siteUrl}</td>
-          <td><button onclick="deleteSite(${i})" class="btn btn-sm btn-danger">Delete</button></td>
-          <td><a href="${webSiteInfo[i].siteUrl}" class="btn btn-sm btn-primary" target="_blank">visit</a></td>
-          </tr>`;
+function clearForm() {
+  websiteNameInput.value = "";
+  websiteURL.value = "";
+}
+// Add Website
+
+function addWebsite() {
+  if (validateWebsiteURL() == true) {
+    let website = {
+      websiteName: websiteNameInput.value,
+      websiteURL: websiteURL.value,
+    };
+    webSiteInfo.push(website);
+    localStorage.setItem("website", JSON.stringify(webSiteInfo));
+    console.log(webSiteInfo);
+    displayWebsite();
+    clearForm();
+  } else {
+    window.alert("Invalid URL");
   }
-  document.getElementById("tableBody").innerHTML = sites;
 }
 
-function clearInputs() {
-  siteNameInput.value = "";
-  urlInput.value = "";
+// Display Data
+
+function displayWebsite() {
+  let website = ``;
+  for (i = 0; i < webSiteInfo.length; i++) {
+    website += `<tr>
+        <td>${i}</td>
+        <td>${webSiteInfo[i].websiteName}</td>
+        <td id="URL">${webSiteInfo[i].websiteURL}</td>
+        
+        <td><button class="btn btn-sm btn-info" onclick="websiteInfo(${i})" >Update</button></td>
+        <td><button onclick="deleteWebsite(${i})"class="btn btn-sm btn-danger">Delete</button></td>
+        <td><a class="btn btn-sm btn-primary" target="_blank" href="${webSiteInfo[i].websiteURL}">Visit</a></td>
+        </tr>`;
+  }
+  document.getElementById(`tableBody`).innerHTML = website;
 }
 
-function deleteSite(deletedIndex) {
+// Delete data
+
+function deleteWebsite(deletedIndex) {
   webSiteInfo.splice(deletedIndex, 1);
   localStorage.setItem("website", JSON.stringify(webSiteInfo));
-  displayWebSites();
+  displayWebsite();
+}
+
+// Validate websiteURL
+
+function validateWebsiteURL() {
+  let regex = /[A-Z][a-z]/;
+  let regex2 = /^(ftp|http|https):(\/\/)/;
+  if (
+    regex.test(websiteNameInput.value) == true &&
+    regex2.test(websiteURLInput.value) == true
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Get Website Info
+
+function websiteInfo(Index) {
+  currentIndex = Index;
+  let currentSite = webSiteInfo[Index];
+  websiteNameInput.value = currentSite.websiteName;
+  websiteURLInput.value = currentSite.websiteURL;
+  addBtn.innerHTML = "Update";
+}
+
+// Update Website URL
+
+function updateWebsite() {
+  let website = {
+    websiteName: websiteNameInput.value,
+    websiteURL: websiteURL.value,
+  };
+  webSiteInfo[currentIndex] = website;
+  localStorage.setItem("website", JSON.stringify(webSiteInfo));
+  displayWebsite();
+  clearForm();
+  addBtn.innerHTML = "Submit";
+}
+
+// Submit Button
+
+addBtn.onclick = function () {
+  if (addBtn.innerHTML == "Submit") {
+    addWebsite();
+  } else {
+    updateWebsite();
+  }
+};
+
+// Search on Websites
+
+function searchUrl(term) {
+  let website = ``;
+  for (i = 0; i < webSiteInfo.length; i++) {
+    if (
+      webSiteInfo[i].websiteURL.toLowerCase().includes(term.toLowerCase()) ==
+      true
+    )
+      website += `<tr>
+            <td>${i}</td>
+            <td>${webSiteInfo[i].websiteName}</td>
+            <td id="URL">${webSiteInfo[i].websiteURL}</td>
+
+            <td><button onclick="deleteWebsite(${i})"class="btn btn-sm btn-danger">Delete</button></td>
+            <td><a class="btn btn-sm btn-primary" target="_blank" href="${webSiteInfo[i].websiteURL}">Visit</a></td>
+            <td><button class="btn btn-sm btn-info">Update</button></td>
+            </tr>`;
+  }
+  document.getElementById(`tableBody`).innerHTML = website;
 }
