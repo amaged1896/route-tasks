62f59865386ec072913da63a34eb8dc2eb5e0b08
amaged1896 <ahmed.maged1896@gmail.com>
2022-10-12 03:12:41 +0200

    "add userLogin Function"

--- Front-end Assignmets/task-10/js/script.js
+++ Front-end Assignmets/task-10/js/script.js
@@ -1,46 +1,33 @@
 const signUpName = document.getElementById("signUpName");
-const signUpPassword = document.getElementById("signUpPassword");
 const signUpEmail = document.getElementById("signUpEmail");
-
+const signUpPassword = document.getElementById("signUpPassword");
 
 const signInEmail = document.getElementById("signInEmail");
 const signInPassword = document.getElementById("signInPassword");
 
 // function Add user and set to localStorage
+let userContainer = [];
 function addUser() {
   let userData = {
     userName: signUpName.value,
     userPass: signUpPassword.value,
     userEmail: signUpEmail.value,
   };
-  
-  localStorage.setItem("userData", JSON.stringify(usersContainer));
-  return userData;
+  userContainer.push(userData);
+  localStorage.setItem("userData", JSON.stringify(userContainer));
 }
 
-// uncommitted yet
+// user Login
 function userLogin() {
-  for (let i = 0; i < usersContainer.length; i++) {
-    const user = usersContainer[i];
+  let users = JSON.parse(localStorage.getItem("userData"));
+  for (let i = 0; i < users.length; i++) {
+    if (
+      users[i].userPass == signInPassword.value &&
+      users[i].userEmail == signInEmail.value
+    ) {
+      window.location.pathname = "/welcome.html";
+    } else {
+      console.log("Invalid Data");
+    }
   }
-
-  // let user = {
-  //   userEmail: signInEmail.value,
-  //   userPass: signInPassword.value,
-  // };
-
-  // if (
-  //   signInEmail.value == JSON.parse(localStorage.getItem("userData")) ||
-  //   signInPassword.value == JSON.parse(localStorage.getItem("userData"))
-  // ) {
-  //   window.location = "welcome.html";
-  // } else {
-  //   console.log("user is invalid");
-  // }
 }
-
-// if(userName.value == storedName && userPw.value == storedPw) {
-//   alert('You are logged in.');
-// }else {
-//   alert('ERROR.');
-// }
