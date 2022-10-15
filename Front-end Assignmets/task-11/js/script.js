243286a4c0debdcc4fbe25a9007c85bd1c9fcc00
amaged1896 <ahmed.maged1896@gmail.com>
2022-10-15 18:19:54 +0200

    "display weather function"

--- Front-end Assignmets/task-11/js/script.js
+++ Front-end Assignmets/task-11/js/script.js
@@ -1,14 +1,13 @@
-// API Key
-// bcc8bbd5f9374cc8975185520221410
-
 let searchLocation = document.getElementById("searchLocation");
-let items = [];
 
-async function getDataFromApi() {
-  let getData = await fetch(
-    "https://api.weatherapi.com/v1/forecast.json?key=bcc8bbd5f9374cc8975185520221410&q=cairo&days=3&aqi=no&alerts=no"
-  );
-  let dataToJson = await getData.json();
-  console.log(dataToJson);
+function displayWeather(apiDegree) {
+  const d = new Date();
+  let currentHour = d.getHours();
+
+  apiDegree.forEach((item, index) => {
+    console.log(item);
+    const currentDegree = item.hour[currentHour].temp_c;
+    document.getElementsByClassName("degree")[index].innerHTML =
+      currentDegree + " C";
+  });
 }
-getDataFromApi();
